bf00d3cd0436f8fa8c477987b8dd8d9d69963feb
SOCCoreDev <89928541+SOCCoreDev@users.noreply.github.com>
2021-10-11 10:45:28 +0500

    added show data window

--- crawler.py
+++ crawler.py
@@ -1,23 +1,89 @@
-import requests
-from bs4 import BeautifulSoup
+import bs4 as bs
+import sys
+import urllib.request
+from PyQt5.QtWebEngineWidgets import QWebEnginePage
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtCore import QUrl
+import pandas as pd
  
+app = QApplication(sys.argv)
+
+class VirtualBrowser(QWebEnginePage):
+    def __init__(self, url):
+        self.app = app
+        QWebEnginePage.__init__(self)
+        self.html = ''
+        self.loadFinished.connect(self._on_load_finished)
+        self.load(QUrl(url))
+        self.app.exec_()
+
+    def _on_load_finished(self):
+        self.html = self.toHtml(self.Callable)
+        print('[INFO] Scrape Finish.')
+
+    def Callable(self, html_str):
+        self.html = html_str
+        self.app.quit()
+
+
 class Crawler:
     def __init__(self):
         self.strings = []
+        self.doc = None
+        self.soup = None
+        self.tables = None
 
-    def scrap_text(self, doc):
-        print("Fetching...")
-        # getting response object
-        res = requests.get(doc)
+    def set_url(self, doc):
+        self.doc = doc
+
+    def clean_text(self, data):
+        li = []
+        data = data.split("\n")
+        for e in data:
+            if e:
+                e = e.encode('ascii',errors='ignore').decode().replace("\t", "").strip()
+                if e:
+                    li.append(e)
+        return li
+
+    def scrape_text(self):
+        print("[INFO] Fetching...")
+        try:
+            page = VirtualBrowser(self.doc)
+            soup = bs.BeautifulSoup(page.html, 'html.parser')
+            self.soup = soup
+            body = soup.body
+            return self.clean_text(body.text)
+        except Exception as e:
+            print("[scrap_text EXCEPTION] ", str(e))
         
-        # Initialize the object with the document
-        soup = BeautifulSoup(res.content, "html.parser")
-        
-        # Get the whole body tag
-        tag = soup.body
-        
-        # Print each string recursively
-        for string in tag.strings:
-            self.strings.append(string)
-        print("Fetching Completed.")
-        return self.strings
+
+    def scrape_tables(self):
+        print("[INFO] Fetching Tables...")
+        try:
+            tbs = self.soup.find_all('table')
+            df = pd.read_html(str(tbs))
+            print("[INFO] Fetching Completed.")
+            if df:
+                return df
+        except Exception as e:
+            print("[scrap_tables EXCEPTION] ", str(e))
+
+    def scrape_media(self):
+        print("[INFO] Fetching Media.")
+        try:
+            images = []
+            videos = []
+            img_tags = self.soup.find_all('img')
+            video_tags = self.soup.find_all('video')
+            for each in img_tags:
+                images.append(each["src"])
+            for each in video_tags:
+                videos.append(each["src"])
+            return images, videos
+        except Exception as e:
+            print("[scrape_media EXCEPTION] ", str(e))
+
+
+#https://pythonprogramming.net/parsememcparseface/
+#https://finance.yahoo.com/quote/%5EGSPC/history?p=%5EGSPC
--- main_page.py
+++ main_page.py
@@ -3,7 +3,19 @@ from tkinter import ttk
 from tkinter import * 
 import random
 import urllib.request
+
+from numpy.lib.type_check import imag
 from crawler import Crawler
+
+
+TEXT = "No Data"
+TABLES = "No Data"
+VIDEOS = "No Data"
+IMAGES = "No Data"
+DATA_WINDOW = None
+SCR = None
+HSCR = None
+LV = None
 
 # this is a function to get the user input from the text input box
 def getInputBoxValue():
@@ -16,15 +28,137 @@ def makeProgress():
 	progessBarOne['value']=progessBarOne['value'] + 1
 	root.update_idletasks()
 
+def destroyer(objects):
+    for each in objects:
+        each.destroy()
+
+def create_data_window_text():
+    global DATA_WINDOW, TEXT, TABLES, SCR, LV, HSCR
+    if SCR and LV and HSCR:
+        destroyer([SCR, LV, HSCR])
+    scrollbar = Scrollbar(DATA_WINDOW, orient=VERTICAL)
+    scrollbar.pack( side = RIGHT, fill = Y )
+
+    hscrollbar = Scrollbar(DATA_WINDOW, orient=HORIZONTAL)
+    hscrollbar.pack( side = BOTTOM, fill = X )
+
+    mylist = Listbox(DATA_WINDOW, yscrollcommand = scrollbar.set,xscrollcommand=hscrollbar.set, width=145, height=25)
+    if isinstance(TEXT, str):
+        TEXT = [TEXT]
+    for line in range(len(TEXT)):
+        mylist.insert(END, str(line) + ") " + TEXT[line])
+
+    mylist.pack( side = LEFT, fill = BOTH)
+    scrollbar.config( command = mylist.yview )
+    hscrollbar.config( command = mylist.xview )
+    SCR = scrollbar
+    LV = mylist
+    HSCR = hscrollbar
+
+def create_data_window_images():
+    global DATA_WINDOW, IMAGES, SCR, LV, HSCR
+    if SCR and LV and HSCR:
+        destroyer([SCR, LV, HSCR])
+    scrollbar = Scrollbar(DATA_WINDOW)
+    scrollbar.pack( side = RIGHT, fill = Y )
+
+    hscrollbar = Scrollbar(DATA_WINDOW, orient=HORIZONTAL)
+    hscrollbar.pack( side = BOTTOM, fill = X )
+
+    mylist = Listbox(DATA_WINDOW, yscrollcommand = scrollbar.set,xscrollcommand=hscrollbar.set,  width=145, height=25)
+    if isinstance(IMAGES, str):
+        IMAGES = [IMAGES]
+    for line in range(len(IMAGES)):
+        mylist.insert(END, str(line) + ") " + IMAGES[line])
+
+    mylist.pack( side = LEFT, fill = BOTH)
+    scrollbar.config( command = mylist.yview )
+    hscrollbar.config( command = mylist.xview )
+    SCR = scrollbar
+    LV = mylist
+    HSCR = hscrollbar
+
+def create_data_window_videos():
+    global DATA_WINDOW, VIDEOS, SCR, LV, HSCR
+    if SCR and LV and HSCR:
+        destroyer([SCR, LV, HSCR])
+    scrollbar = Scrollbar(DATA_WINDOW)
+    scrollbar.pack( side = RIGHT, fill = Y )
+
+    hscrollbar = Scrollbar(DATA_WINDOW, orient=HORIZONTAL)
+    hscrollbar.pack( side = BOTTOM, fill = X )
+
+    mylist = Listbox(DATA_WINDOW, yscrollcommand = scrollbar.set,xscrollcommand=hscrollbar.set,  width=145, height=25)
+    if isinstance(VIDEOS, str):
+        VIDEOS = [VIDEOS]
+    for line in range(len(VIDEOS)):
+        mylist.insert(END, str(line) + ") " + VIDEOS[line])
+
+    mylist.pack( side = LEFT, fill = BOTH)
+    scrollbar.config( command = mylist.yview )
+    hscrollbar.config( command = mylist.xview )
+    SCR = scrollbar
+    LV = mylist
+    HSCR = hscrollbar
+
+def create_data_window_tables():
+    global DATA_WINDOW, TABLES, SCR, LV, HSCR
+    if SCR and LV and HSCR:
+        destroyer([SCR, LV, HSCR])
+    scrollbar = Scrollbar(DATA_WINDOW)
+    scrollbar.pack( side = RIGHT, fill = Y )
+
+    hscrollbar = Scrollbar(DATA_WINDOW, orient=HORIZONTAL)
+    hscrollbar.pack( side = BOTTOM, fill = X )
+
+    mylist = Listbox(DATA_WINDOW, yscrollcommand = scrollbar.set,xscrollcommand=hscrollbar.set,  width=145, height=25)
+    if isinstance(TABLES, str):
+        TABLES = [TABLES]
+    for line in range(len(TABLES)):
+        mylist.insert(END, "Table# {}".format(line+1))
+        mylist.insert(END, "_______________________")
+        for i in range(len(TABLES[line])):
+            mylist.insert(END, TABLES[line].iloc[i, :])
+
+    mylist.pack( side = LEFT, fill = BOTH)
+    scrollbar.config( command = mylist.yview )
+    hscrollbar.config( command = mylist.xview )
+    SCR = scrollbar
+    LV = mylist
+    HSCR = hscrollbar
+
 
 # this is the function called when the button is clicked
 def btnClickFunction():
-	print('clicked')
+    global  DATA_WINDOW
+    print('clicked')
+    window = tk.Toplevel(root)
+    disp_wind = tk.Canvas(window, height=592, width=891)
+    disp_wind.pack()
+    disp_wind.configure(background='#FFFFFF')
+    text_data = Button(window, text='Text Data', bg='#00FF00', font=('arial', 12, 'normal'), command=create_data_window_text, state="normal")
+    text_data.place(x=100+120, y=50)
+    tables_data = Button(window, text='Tablular Data', bg='#00FF00', font=('arial', 12, 'normal'), command=create_data_window_tables, state="normal")
+    tables_data.place(x=185+120, y=50)
+    images_data = Button(window, text='Media - Images', bg='#00FF00', font=('arial', 12, 'normal'), command=create_data_window_images, state="normal")
+    images_data.place(x=295+120, y=50)
+    videos_data = Button(window, text='Media - Videos', bg='#00FF00', font=('arial', 12, 'normal'), command=create_data_window_videos, state="normal")
+    videos_data.place(x=420+120, y=50)
+    ttk.Separator(window, orient='horizontal').place(x=0, y=85, relwidth=6)
+    data_window = tk.Canvas(window, height=450, width=891)
+    data_window.place(x=0, y=95)
+    DATA_WINDOW = data_window
+    # data_label = Label(window, text=TEXT, fg='#00FF00' , font=('arial', 12, 'normal'))
+    # data_label.place(x=0, y=95)
+    # # myentry = ttk.Entry(data_window, textvariable=TEXT,fg='#00FF00', state='readonly')
 
+    # myscroll = ttk.Scrollbar(data_window, orient='vertical', command=data_window.xview)
+    # data_window.config(xscrollcommand=myscroll.set)
+    
 
-# this is the function called when the button is clicked
-def btnClickFunction():
-	print('clicked')
+    # data_window.grid()
+    # myentry.place(x=0, y=10)
+    # myscroll.grid(row=2, sticky='ew')
 
 def validate_url(url):
     try:
@@ -36,7 +170,7 @@ def validate_url(url):
 
 def btnStartScraping():
     is_valid_url = False
-    global error_msg_lbl, status_msg_lbl
+    global error_msg_lbl, status_msg_lbl, TEXT, TABLES, IMAGES, VIDEOS
     status_msg_lbl.destroy()
     status_msg_lbl = Label(root, text='', fg='#00FF00', bg='#FFFFFF', font=('arial', 12, 'normal'))
     status_msg_lbl.config(text='Validating URL')
@@ -59,10 +193,29 @@ def btnStartScraping():
     status_msg_lbl.place(x=300, y=220)
 
     if is_valid_url:
-        text = crawl.scrap_text(url)
+        crawl.set_url(url)
+        text = crawl.scrape_text()
+        if text:
+            TEXT = text
         print(text)
+        tables = crawl.scrape_tables()
+        if tables:
+            TABLES = tables
+        print(tables)
+        images, videos = crawl.scrape_media()
+        if images:
+            IMAGES = images
+        if videos:
+            VIDEOS = videos
+        print("IMAGES ", images)
+        print("VIDEOS ", videos)
 
-
+    # with open("test_text.txt", "w", encoding='utf8', errors='ignore')as f:
+    #     f.write(text)
+    show_data.config(state="normal")
+    save_data.config(state="normal")
+    
+    
 def gui_status(prompter=None):
     if prompter:
         return prompter + "..."
@@ -123,14 +276,28 @@ error_msg_lbl.place(x=300, y=250)
 # This is the section of code which creates a progress bar
 # progessBarOne=ttk.Progressbar(root, style='progessBarOne.Horizontal.TProgressbar', orient='horizontal', length=236, mode='determinate', maximum=100, value=1)
 # progessBarOne.place(x=98, y=160)
+def on_enter_show_data(e):
+    show_data['background'] = '#006400'
 
+def on_leave_show_data(e):
+    show_data['background'] = '#00FF00'
+
+def on_enter_save_data(e):
+    save_data['background'] = '#006400'
+
+def on_leave_save_data(e):
+    save_data['background'] = '#00FF00'
 
 # This is the section of code which creates a button
-Button(root, text='Show Data', bg='#00FF00', font=('arial', 12, 'normal'), command=btnClickFunction, state="disabled").place(x=272, y=413)
-
+show_data = Button(root, text='Show Data', bg='#00FF00', font=('arial', 12, 'normal'), command=btnClickFunction, state="normal")#disabled
+show_data.place(x=272, y=413)
+show_data.bind("<Enter>", on_enter_show_data)
+show_data.bind("<Leave>", on_leave_show_data)
 
 # This is the section of code which creates a button
-Button(root, text='Save without Show', bg='#00FF00', font=('arial', 12, 'normal'), command=btnClickFunction, state="disabled").place(x=370, y=413)
-
+save_data = Button(root, text='Save without Show', bg='#00FF00', font=('arial', 12, 'normal'), state="normal")
+save_data.place(x=370, y=413)
+save_data.bind("<Enter>", on_enter_save_data)
+save_data.bind("<Leave>", on_leave_save_data)
 
 root.mainloop()
